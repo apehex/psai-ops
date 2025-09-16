31e0a6ab5b56fa6c0a916af4ee807f642b3bed88
apehex <apehex@protonmail.com>
2025-09-16 18:11:04 +0200

    Handle all the HTTP errors

--- psaiops/elements/data.py
+++ psaiops/elements/data.py
@@ -8,10 +8,13 @@ HF_URL = 'https://huggingface.co/api/quicksearch?q={target}&type={label}&limit={
 
 def query_huggingface(target: str, label: str='model', limit: int=16, endpoint: str=HF_URL) -> list:
     __results = []
-    # query HF
-    __response = requests.get(endpoint.format(target=target, label=label, limit=limit))
-    # no error
-    if __response.status_code == 200:
-        __results = [__d['id'] for __d in __response.json().get(f'{label}s', [])]
+    # handle all the errors
+    try:
+        # query HF
+        __response = requests.get(endpoint.format(target=target, label=label, limit=limit))
+        # filter by type ('models' / 'datasets' / 'spaces')
+        __results = [__d.get('id', '') for __d in __response.json().get(f'{label}s', [])]
+    except:
+        __results = []
     # list of strings
     return __results
