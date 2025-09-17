ac94df1a94fe13d6d719d3fae2abca2644a7786a
apehex <apehex@protonmail.com>
2025-09-17 08:26:18 +0200

    Normalize the data type argument

--- psaiops/elements/data.py
+++ psaiops/elements/data.py
@@ -8,12 +8,15 @@ HF_URL = 'https://huggingface.co/api/quicksearch?q={target}&type={label}&limit={
 
 def query_huggingface(target: str, label: str='model', limit: int=16, endpoint: str=HF_URL) -> list:
     __results = []
-    # handle all the errors
+    # make sure the label has no trailing "s"
+    __label = label.rstrip('s').strip(' ')
+    # the HTTP request or the parsing may fail
     try:
         # query HF
-        __response = requests.get(endpoint.format(target=target, label=label, limit=limit))
+        __response = requests.get(endpoint.format(target=target, label=__label, limit=limit))
         # filter by type ('models' / 'datasets' / 'spaces')
-        __results = [__d.get('id', '') for __d in __response.json().get(f'{label}s', [])]
+        __results = [__d.get('id', '') for __d in __response.json().get(f'{__label}s', [])]
+    # ignore all the errors
     except:
         __results = []
     # list of strings
