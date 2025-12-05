d2003029f7463be1e7ba7fe8a2db79307c601372
apehex <17317183+apehex@users.noreply.github.com>
2025-12-05 14:17:12 +0100

    Compute the router scores

--- psaiops/score/router/lib.py
+++ psaiops/score/router/lib.py
@@ -64,3 +64,57 @@ def generate_token_ids(
             use_cache=True)
     # full sequence
     return __outputs.sequences # (1, T)
+
+# COMPUTE ########################################################################
+
+def compute_router_weights(
+    model_obj: object,
+    token_obj: torch.Tensor,
+) -> torch.Tensor:
+    # process the full sequence
+    with torch.no_grad():
+        __outputs = model_obj(
+            input_ids=token_obj,
+            output_attentions=False,
+            output_router_logits=True,
+            return_dict=True)
+    # stack all the layer outputs L * (B, T, E) => (L, B, T, E)
+    __logits = torch.stack(__outputs.router_logits, dim=0)
+    # turn the logits into expert probabilities
+    return torch.softmax(__logits, dim=-1)
+
+# REDUCE #######################################################################
+
+def reduce_router_weights(
+    router_data: torch.Tensor,
+    token_idx: int, # -1 => avg over all tokens
+) -> torch.Tensor:
+    # parse
+    __layer_dim, __batch_dim, __token_dim, __expert_dim = tuple(router_data.shape) # L, B, T, E
+    __token_idx = min(token_idx, __token_dim - 1)
+    # select the relevant data along each axis
+    __token_slice = slice(0, __token_dim) if (__token_idx < 0) else slice(__token_idx, __token_idx + 1)
+    # filter the data
+    __data = router_data[slice(None), slice(None), __token_slice, slice(None)]
+    # reduce all the axes but the last
+    return __data.mean(dim=(1, 2), keepdim=False)
+
+# FORMAT #########################################################################
+
+def postprocess_router_weights(
+    router_data: torch.Tensor, # (L, E)
+) -> list:
+    return 100.0 * torch.softmax(router_data, dim=-1)
+
+# POSTPROCESS ####################################################################
+
+def postprocess_token_ids(
+    tokenizer_obj: object,
+    token_obj: torch.Tensor,
+) -> list:
+    # remove the batch axis
+    __indices = token_obj.squeeze().tolist()
+    # back to token strings
+    __tokens = tokenizer_obj.convert_ids_to_tokens(__indices)
+    # normalize the tokens
+    return [__t.replace(chr(0x0120), ' ').replace(chr(0x010a), '\n') for __t in __tokens]
